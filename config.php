9aca1366d7da5d7ef7f08b70365a0dd24c9e2790
Ethan Patterson <eryanpatterson@gmail.com>
2021-09-16 10:40:34 -0400

    Testing

--- config.php
+++ config.php
@@ -1,16 +1,12 @@
 <?php
     session_start();
 
-    $host = 'ec2-44-193-228-249.compute-1.amazonaws.com';
-    $user = 'dmfcuuuklliuqc';
-    $password = $_ENV['DB_PASSWORD'];
-    $db = 'dfo8dbpa5aijgo';
-    $port = 5432;
+    $test_string = getenv("TEST_STRING");
 
     $conn = pg_connect(getenv("DB_URI"));
 
     if (!$conn) {
-        die("Error connecting to ye olde database: " . mysqli_connect_error());
+        die("$test_string" . mysqli_connect_error());
     }
 
     define ('ROOT_PATH', realpath(dirname(__FILE__)));
