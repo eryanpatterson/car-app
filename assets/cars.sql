b47e61943421f16973c7212f31807c3c9b03b188
Ethan Patterson <eryanpatterson@gmail.com>
2021-09-17 13:21:06 -0400

    Add to db

--- assets/cars.sql
+++ assets/cars.sql
@@ -1,14 +1,20 @@
-UPDATE `makes` SET `logo` = 'https://www.pngfind.com/pngs/m/110-1103747_honda-logo-png-png-transparent-png.png' WHERE `makes`.`id` = 2;
+UPDATE `makes` SET `logo` = 'https://www.codinter.com/en/wp-content/uploads/sites/2/2018/01/logo-toyota-256x256.png' WHERE `makes`.`id` = 2;
 
-INSERT INTO `styles` (`id`, `style_name`, `slug`) VALUES
-(2, 'Mid-Size Sedan', 'mid-size-sedan'),
-(3, 'Full-Size Pickup', 'full-size-pickup');
+UPDATE `makes` SET `logo` = 'https://www.freeiconspng.com/thumbs/honda-logo-png/honda-icon-13.png' WHERE `makes`.`id` = 5;
+UPDATE `cars` SET `image` = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-honda-accord-hybrid-109-edit-1604961241.jpg?crop=0.591xw:0.499xh;0.0962xw,0.501xh&resize=2048:*' WHERE `cars`.`id` = 6; 
 
 
-INSERT INTO `cars` (`id`, `make`, `style`, `model`, `image`, `slug`, `msrp`, `c_and_d`, `maker_site`) VALUES
-(5, 'Toyota', 'Mid-Size Sedan', 'Camry', 'https://www.toyota.com/imgix/responsive/images/gallery/photos-videos/2021/camry/CAH_MY21_0003_V001.png?w=2880&q=90&fm=jpg&fit=max&cs=strip&bg=fff&auto=compress', 'camry', '$26,000 - $33,300', 'https://www.caranddriver.com/toyota/camry', 'https://www.toyota.com/camry/2021/'),
-(6, 'Honda', 'Mid-Size Sedan', 'Accord', 'https://automobiles.honda.com/-/media/Honda-Automobiles/Vehicles/2021/Accord-Sedan/Gallery/Responsive/MY21-Accord-Horsepower-Torque-1400-1x-A.jpg', 'accord', '$26,000 - $37,900', 'https://www.caranddriver.com/honda/accord', 'https://automobiles.honda.com/accord-sedan'),
-(7, 'Mazda', 'Mid-Size Sedan', 'Mazda-6', 'https://www.mazdausa.com/siteassets/vehicles/2021/mazda6/environmental-360/sport/soul-red-crystal/e360-my21-mazda6-sport-soulredcrystal-023.jpg#default?w=1480', 'mazda-6', '$25,500 - $36,900', 'https://www.caranddriver.com/mazda/mazda-6', 'https://www.mazdausa.com/vehicles/2021-mazda6'),
-(8, 'Volkswagen', 'Mid-Size Sedan', 'Passat', 'https://www.vw.com/api/assets/onehub_pkw/us/en/gallery/passat/2022/exterior/VW_NGW6_Showroom_Passat_Gallery_Exterior-5.jpg', 'passat', '$25,000 - $31,300', 'https://www.caranddriver.com/volkswagen/passat', 'https://www.vw.com/en/models/passat.html'),
-(9, 'Ford', 'Full-Size Pickup', 'F-150', 'https://www.ford.com/is/image/content/dam/vdm_ford/live/en_us/ford/nameplate/f-150/2021/collections/dm/21_FRD_F15_51223.tif?croppathe=1_3x2&wid=1440', 'f-150', '$31,000 - $72,900', 'https://www.caranddriver.com/ford/f-150', 'https://www.ford.com/trucks/f150/');
+INSERT INTO `images` (`id`, `src`, `car`) VALUES
+(13, 'https://www.toyota.com/imgix/responsive/images/gallery/photos-videos/2021/camry/CAH_MY21_0003_V001.png?w=2880&q=90&fm=jpg&fit=max&cs=strip&bg=fff&auto=compress', 'camry'),
+(14, 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-honda-accord-hybrid-109-edit-1604961241.jpg?crop=0.591xw:0.499xh;0.0962xw,0.501xh&resize=2048:*', 'accord'),
+(15, 'https://www.mazdausa.com/siteassets/vehicles/2021/mazda6/environmental-360/sport/soul-red-crystal/e360-my21-mazda6-sport-soulredcrystal-023.jpg#default?w=1480', 'mazda-6'),
+(16, 'https://www.vw.com/api/assets/onehub_pkw/us/en/gallery/passat/2022/exterior/VW_NGW6_Showroom_Passat_Gallery_Exterior-5.jpg', 'passat'),
+(17, 'https://www.ford.com/is/image/content/dam/vdm_ford/live/en_us/ford/nameplate/f-150/2021/collections/dm/21_FRD_F15_51223.tif?croppathe=1_3x2&wid=1440', 'f-150'),
+(18, 'https://www.vw.com/api/assets/onehub_pkw/us/en/gallery/passat/2022/exterior/VW_NGW6_Showroom_Passat_Gallery_Exterior-9.jpg/renditions/original', 'passat'),
+(19, 'https://www.vw.com/api/assets/onehub_pkw/us/en/gallery/passat/2022/exterior/VW_NGW6_Showroom_Passat_Gallery_Exterior-6.jpg/renditions/original', 'passat'),
+(20, 'https://www.mazdausa.com/siteassets/vehicles/2021/mazda6/vlp/gallery-slider/2021-mazda-6-sedan-drivers-car.jpg?w=1800', 'mazda-6'),
+(21, 'https://www.toyota.com/imgix/responsive/images/gallery/photos-videos/2021/camry/CAM_MY20_0034_V003.png?w=2880&q=90&fm=jpg&fit=max&cs=strip&bg=fff&auto=compress', 'camry'),
+(22, 'https://www.toyota.com/imgix/responsive/images/gallery/photos-videos/2021/camry/CAM_MY19_0018_V003.png?w=2880&q=90&fm=jpg&fit=max&cs=strip&bg=fff&auto=compress', 'camry'),
+(23, 'https://media.ed.edmunds-media.com/honda/accord/2021/oem/2021_honda_accord_sedan_sport_fq_oem_1_1600.jpg', 'accord'),
+(24, 'https://www.ford.com/is/image/content/dam/vdm_ford/live/en_us/ford/nameplate/f-150/2021/collections/dm/21_FRD_F15_51172.tif?croppathe=1_3x2&wid=1440', 'f-150');
 
